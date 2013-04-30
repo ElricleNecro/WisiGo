9e7cdfa44f1db7b7f2fa3de74d0dfd89781a43a8
Guillaume Plum <guiplum@gmail.com>
2013-04-30 15:32:12 +0200

    Begin to write Create (octree) function.

--- octree.go
+++ octree.go
@@ -1,12 +1,35 @@
 package wisur
 
+import rg "ReadGadget"
+
 type Node struct {
 	Parent *Node
 	Fils   [8]*Node
+	Center [3]float32
+	Size   float32
+	Part   []rg.Particule
+	level  int64
 }
 
-func New() Node {
+func New(part []rg.Particule, Center [3]float32, side float32) *Node {
 	var res Node
 
-	return res
+	res.Parent = nil
+	for i, _ := range res.Fils {
+		res.Fils[i] = nil
+	}
+
+	res.Size = side
+	res.Center = Center
+	res.level = 0
+
+	return &res
+}
+
+func (e *Node) Create(NbMin int32) {
+	for i, v range e.Fils {
+		if i & 2 {
+			tmp = New()
+		}
+	}
 }
