b54b9d05da11c102ec2efe0dfceb5936401a0482
ElricleNecro <guiplum@gmail.com>
2013-07-04 17:01:54 +0200

    Types declaration move into types.go

--- octree/types.go
+++ octree/types.go
@@ -1,5 +1,16 @@
 package octree
 
+import rg "github.com/ElricleNecro/WisiGo/ReadGadget"
+
+//These Node structure will contain every needed informations about Tree Node.
+type Node struct {
+	Parent, Fils, Frere *Node          //These is the Main Node to Now : the parent, the first son and the next brother.
+	Center              [3]float32     //Center of the cube.
+	Size                float32        //Side size of the cube.
+	Part                []rg.Particule //List of the particle contain in the node.
+	level               int64          //Level of the tree.
+}
+
 type WriteStringer interface {
 	WriteString(s string) (ret int, err error)
 }
